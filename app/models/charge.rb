5783bb4fc6d9514eb071ac934903b77eba07ab10
Richard Bluestein <richard@bluestein.com>
2014-01-20 16:13:48 -0600

    added status method to model and added seed for create field

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -1,3 +1,14 @@
 class Charge < ActiveRecord::Base
   belongs_to :customer
+
+  def status
+    if self.paid == true && self.refunded == false
+      "successful"
+    elsif self.paid == true && self.refunded == true
+      "disputed"
+    else
+      "failed"
+    end
+
+  end
 end
