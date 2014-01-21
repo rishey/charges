ba6298ae531099cf5fd1b6ba69fb08c536652d58
Richard Bluestein <richard@bluestein.com>
2014-01-21 17:09:52 -0600

    added validations for charge

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -1,6 +1,9 @@
 class Charge < ActiveRecord::Base
   belongs_to :customer
   before_save :compute_status
+  validates :created, :amount, :currency, :customer_id,  presence: true
+  validates_inclusion_of :paid, :refunded, :in => [true, false]
+
 
   def compute_status
     if self.paid == true && self.refunded == false
