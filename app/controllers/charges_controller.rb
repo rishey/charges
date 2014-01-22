34c93c3ce13017c07c3a099a931c98fc6b7602bf
Richard Bluestein <richard@bluestein.com>
2014-01-21 21:44:32 -0600

    refactored view with controller further

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,9 +1,10 @@
 class ChargesController < ApplicationController
 
   def index
-    @failed = Charge.where(status:"failed")
-    @disputed = Charge.where(status:"disputed")
-    @successful = Charge.where(status:"successful")
+    failed = Charge.where(status:"failed")
+    disputed = Charge.where(status:"disputed")
+    successful = Charge.where(status:"successful")
+    @all_charges = [failed, disputed, successful]
   end
 
 end
